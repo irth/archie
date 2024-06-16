7dd3941c0b163d8d0cc5f696e802339fead74766
Wojciech Kwolek <wojciech@kwolek.io>
2024-06-16 19:30:33 +0200

    default dark mode

--- static/js/themetoggle.js
+++ static/js/themetoggle.js
@@ -2,11 +2,11 @@ function setTheme(mode) {
     localStorage.setItem("theme-storage", mode);
     if (mode === "dark") {
         document.getElementById("darkModeStyle").disabled=false;
-        document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"sun\"></i>";
+        document.getElementById("dark-mode-toggle").innerHTML = `<a href="#">light</a>`
         feather.replace()
     } else if (mode === "light") {
         document.getElementById("darkModeStyle").disabled=true;
-        document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"moon\"></i>";
+        document.getElementById("dark-mode-toggle").innerHTML = `<a href="#">dark</a>`
         feather.replace()
     }
 }
@@ -19,5 +19,5 @@ function toggleTheme() {
     }
 }
 
-var savedTheme = localStorage.getItem("theme-storage") || "light";
+var savedTheme = localStorage.getItem("theme-storage") || "dark";
 setTheme(savedTheme);
